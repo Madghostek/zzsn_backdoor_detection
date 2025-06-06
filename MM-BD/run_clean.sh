f607a75368f44ab89ab5ada2cad1559694a9e55a
Bartek <bartlomiejk@vp.pl>
2025-06-06 23:25:01 +0200

    Prepared branch for Neural Cleanse, ResNet model

--- MM-BD/run_clean.sh
+++
@@ -1,9 +0,0 @@
-echo ----------------------------start training ------------------------------
-
-for((integer = 18; integer <= 9; integer ++))
-do
-
-  foo2="python train.py --model_dir clean$integer"
-  $foo2
-done
-
