e33cd4550bf90b2cd97133c2d0c7842c013270b1
Bartek <bartlomiejk@vp.pl>
2025-06-06 22:55:00 +0200

    Added support for CLIP model

--- K-ARM/utils.py
+++ K-ARM/utils.py
@@ -3,6 +3,8 @@
 import torch 
 import torchvision.models as models
 from torch import nn
+import open_clip
+from torchvision import transforms
 
 # print configurations
 def print_args(opt):
@@ -18,15 +20,39 @@ def print_args(opt):
 
 # load model on device, get number of classes
 def loading_models(args):
-    
-    net = models.resnet50(weights = models.ResNet50_Weights.IMAGENET1K_V1)
-    net.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1, bias=False)
-    net.fc = nn.Linear(net.fc.in_features, args.num_classes)
-    net = net.to(args.device)
-    model = torch.load(args.model_filepath, map_location=args.device) #original
-    net.load_state_dict(model)
+    if args.model_name == "ResNet":
+        net = models.resnet50(weights = models.ResNet50_Weights.IMAGENET1K_V1)
+        net.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1, bias=False)
+        net.fc = nn.Linear(net.fc.in_features, args.num_classes)
+        net = net.to(args.device)
+        model = torch.load(args.model_filepath_1, map_location=args.device) #original
+        net.load_state_dict(model)
 
-    return net
+        transform = transforms.Compose([
+            transforms.Resize((64, 64)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+    if args.model_name == "CLIP":
+        import sys
+        sys.path.append('../')
+        sys.path.append('../BadMerging/')
+        sys.path.append('../BadMerging/src')
+
+        from BadMerging.src.modeling import ImageClassifier
+
+
+        model = torch.load(args.model_filepath_1 ,weights_only=False).to(args.device)
+        encoder, train_preprocess, val_preprocess = open_clip.create_model_and_transforms("ViT-B-32", pretrained=None)
+        model.model.transformer = encoder.transformer.to(args.device)
+
+        classification_head = torch.load(args.model_filepath_2, weights_only=False).to(args.device)
+        net = ImageClassifier(model, classification_head)
+
+        transform = train_preprocess
+
+    return net, transform
 
 
 
