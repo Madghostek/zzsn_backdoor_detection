36b457b8c327a8071444d148bcf3c407797b7c4c
Bartek <bartlomiejk@vp.pl>
2025-06-07 00:53:42 +0200

    Created branch for ResNet K-ARM

--- MM-BD/run_detect.sh
+++
@@ -1,8 +0,0 @@
-echo ----------------------------start detecting ------------------------------
-
-for((integer = 0; integer <= 9; integer ++))
-do
-  foo1="python univ_bd.py --model_dir model$integer"
-  $foo1
-done
-
